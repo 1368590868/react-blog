950c4ddd91754bee144287ecd029af22af7c266b
ranlin <ranlin@novasoftware.cn>
2023-08-16 19:56:58 +0800

    feat 优化打包

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import commonjs from 'vite-plugin-commonjs'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), commonjs()],
+  plugins: [react(), commonjs(), viteCompression()],
   resolve: {
     alias: {
       '@': '/src',
